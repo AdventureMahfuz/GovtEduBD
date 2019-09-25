bf7bde988da8005c4959026bf76546aad6e7e301
AdventureMahfuz <adventure.mahfuz@gmail.com>
2019-09-25 19:01:07 +0600

    designed gallery page, single page and responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,6 +17,40 @@
         });
 
 
+
+        //mobile menu custom js
+        $(".mobile-menu-btn").on("click", function () {
+            $('ul.mobile-menu').css({
+                'right': '0',
+                'opacity': '1',
+                'transition': 'all .5s',
+            });
+        });
+        $("#close").on("click", function () {
+            $('ul.mobile-menu').css({
+                'right': '-100%',
+                'opacity': '0',
+                'transition': 'all .8s',
+            });
+        });
+        $("ul.mobile-menu li a").on("click", function () {
+            $('ul.mobile-menu').css({
+                'right': '-100%',
+                'opacity': '0',
+                'transition': 'all .8s',
+            });
+        });
+
+        /*mobile gallery filter items custom dropdown*/
+        $('ul#mobile_filter li').click(function () {
+            $('#select-gallery-item').html($(this).attr('data-value'));
+            $('ul#mobile_filter').css("display", "none");
+        });
+
+        $('.mobile-gallery-filter-menu p').click(function () {
+            $('ul#mobile_filter').css("display", "block");
+        });
+
         //hero slider
         $('.slider-area').owlCarousel({
             animateIn: 'fadeIn',
@@ -46,17 +80,6 @@
                 }
             }
         });
-        //slider animation
-        /*$('#slider').on('translate.owl.carousel',function () {
-            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').removeClass('animated fadeInUp').css('opacity','0');
-            //$('.hero-cont-btn a').removeClass('animated slideInLeft').css('opacity','0');
-            //$('.hero-more-link a').removeClass('animated slideInRight').css('opacity','0');
-        });
-        $('#slider').on('translated.owl.carousel',function () {
-            $('.item-content h1, .item-content p, .hero-cont-btn a, .hero-more-link a').addClass('animated fadeInUp').css('opacity','0');
-            //$('.hero-cont-btn a').addClass('animated slideInLeft').css('opacity','1');
-            //$('.hero-more-link a').addClass('animated slideInRight').css('opacity','1');
-        });*/
 
 
         //teacher slider
